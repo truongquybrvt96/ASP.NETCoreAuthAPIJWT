dbdf8f410e2cec4ba5a942bccaa1e8c9a5578255
truongquybrvt96 <truongquybrvt96@gmail.com>
2019-05-20 00:41:00 +0700

    Add files via upload

---
+++ AuthAPI/Entities/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthAPI.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base (options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
